7f785103be35289cd96d8cbac7c2e5a4013c8eba
shouzy <82171453+realshouzy@users.noreply.github.com>
2023-09-20 00:29:43 +0200

    Adjust ast vs cst

--- src/ast_vs_cst.py
+++ src/ast_vs_cst.py
@@ -6,19 +6,23 @@ import ast
 import libcst as cst
 import libcst.tool as cst_tool
 
-source1: str = "a: int = 1 + 2"
-source2: str = "a: int = (1 + 2)"
-
 
 def main() -> int:
     """Run the program."""
+    source1: str = "a: int = 1 + 2"
+    source2: str = "a: int = (1 + 2)  # some code comment"
+
     print(f"Syntax trees of {source1 = }:")
     print(f"{ast.dump(ast.parse(source1), indent=2)}")
-    print(f"\n{cst_tool.dump(cst.parse_statement(source1))}")
+    print(
+        f"\n{cst_tool.dump(cst.parse_statement(source1), show_syntax=True, show_whitespace=True)}",  # noqa: E501 # pylint: disable=C0301
+    )
 
     print(f"Syntax trees of {source2 = }:")
     print(f"{ast.dump(ast.parse(source2), indent=2)}")
-    print(f"\n{cst_tool.dump(cst.parse_statement(source2))}")
+    print(
+        f"\n{cst_tool.dump(cst.parse_statement(source2), show_syntax=True, show_whitespace=True)}",  # noqa: E501 # pylint: disable=C0301
+    )
 
     return 0
 
