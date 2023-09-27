576954a54655ecef8065343c8c29b4c76fa8f070
shouzy <82171453+realshouzy@users.noreply.github.com>
2023-09-27 11:49:29 +0200

    Make minor fixes

--- src/example1.py
+++ src/example1.py
@@ -1,13 +1,10 @@
 """Example Nr. 1."""
 from __future__ import annotations
 
-# def square(num: int) -> int:
-#     """Calculate the square of the given integer."""
-#     result: int = num * num
-#     return result
+# pylint: skip-file
 
 
 def square(num: int) -> int:
     """Calculate the square of the given integer."""
-    result: int = num * num  # caluclate the square and store it in result
+    result: int = num * num
     return result
